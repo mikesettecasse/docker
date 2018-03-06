a78e55cacbe13dc276dfc94210a35a6a719b5fbe
Diego Rodriguez <diego@qwavee.com>
2018-03-06 09:46:52 -0300

    cambios en la instalación de Drupal 8 Lightning

--- bashfiles/php_script.sh
+++ bashfiles/php_script.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 cd /app
-composer create-project acquia/lightning-project /app
+composer clear-cache
+echo "{}" > ~/.composer/composer.json
+composer create-project acquia/lightning-project /app --no-install --no-interaction  -vvv --ignore-platform-reqs --prefer-dist  --no-progress
+composer update -vvv
 cp /app/docroot/sites/default/default.settings.php /app/docroot/sites/default/settings.php
 mkdir -p /app/dockroot/sites/default/files/translations
 chmod 777 -R /app
