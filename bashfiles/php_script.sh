6032b7ec737f149b217cf07db373d04319035353
Diego Rodriguez <diego@qwavee.com>
2018-03-06 20:37:29 -0300

    logic to differentiate whether the app folder has drupal code or not

--- bashfiles/php_script.sh
+++ bashfiles/php_script.sh
@@ -1,12 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-cd /app
-composer clear-cache
-echo "{}" > ~/.composer/composer.json
-composer create-project acquia/lightning-project /app --no-install --no-interaction  -vvv --ignore-platform-reqs --prefer-dist  --no-progress
-composer update -vvv
-cp /app/docroot/sites/default/default.settings.php /app/docroot/sites/default/settings.php
-mkdir -p /app/dockroot/sites/default/files/translations
-chmod 777 -R /app
+if [ "$(ls -A /app)" ]; then
+     	echo "Folder app has code"
+else
+	cd /app
+	composer clear-cache
+	echo "{}" > ~/.composer/composer.json
+	composer create-project acquia/lightning-project /app --no-install --no-interaction  -vvv --ignore-platform-reqs --prefer-dist  --no-progress
+	composer update -vvv
+	cp /app/docroot/sites/default/default.settings.php /app/docroot/sites/default/settings.php
+	mkdir -p /app/dockroot/sites/default/files/translations
+	chmod 777 -R /app
+fi
 
+echo "Running Php FPM Server" 
 php-fpm
