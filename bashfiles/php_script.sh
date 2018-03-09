2cf8a4788685a3b81e1f645df6ddf91019f54e47
Diego Rodriguez <diego@qwavee.com>
2018-03-09 10:45:05 -0300

    Fix type error en bach file

--- bashfiles/php_script.sh
+++ bashfiles/php_script.sh
@@ -9,7 +9,7 @@ else
 	composer create-project acquia/lightning-project /app --no-install --no-interaction  -vvv --ignore-platform-reqs --prefer-dist  --no-progress
 	composer update -vvv
 	cp /app/docroot/sites/default/default.settings.php /app/docroot/sites/default/settings.php
-	mkdir -p /app/dockroot/sites/default/files/translations
+	mkdir -p /app/docroot/sites/default/files/translations
 	chmod 777 -R /app
 fi
 
